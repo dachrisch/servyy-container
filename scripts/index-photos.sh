a7be14ebf325c65dbdb99e63ecf789c9f575926c
dachrisch <christian.daehn@gmail.com>
2022-10-26 16:07:45 +0200

    scripts to enable services

--- scripts/index-photos.sh
+++ scripts/index-photos.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-pushd $HOME/photoprism
+pushd $HOME/servyy-container/photoprism
 current_path=$( date +"cloudy/%Y/%m" )
 docker-compose exec -T photoprism photoprism index $current_path
