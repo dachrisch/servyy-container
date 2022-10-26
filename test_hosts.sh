7588ac2a63579c4547456edcabaa834069c7a74f
dachrisch <christian.daehn@gmail.com>
2022-10-26 14:50:02 +0200

    init script

---
+++ test_hosts.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+function get_http_code() {
+  domain=$1
+  url=$2
+
+  if [ -z "$url" ];then url="https://$domain";fi
+
+  echo $( curl -Is -o /dev/null -w"%{http_code}" --resolve $domain:443:localhost $url )
+}
+
+test_domain() {
+  domain=$1
+  echo -n "testing [$domain]..."
+  if [ "200" = $( get_http_code $domain) ];then
+    echo "[OK]"
+  else
+    echo "[FAILED]"
+  fi
+}
+
+test_domain www.xn--hfer-architekten-mwb.de
+test_domain xn--hfer-architekten-mwb.de
+test_domain achim.servyy.duckdns.org
+test_domain bumbleflies.servyy.duckdns.org
+test_domain git.servyy.duckdns.org
+
