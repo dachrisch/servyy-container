d5818a1a08f4e7446f3231dec5645b71b56a495d
dachrisch <christian.daehn@gmail.com>
2022-10-26 14:50:02 +0200

    init script

---
+++ startup_all_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+services=(traefik photo bumbleflies achim_hoefer git)
+
+for service in ${services[@]};do
+  systemctl --user start docker-$service
+  systemctl --user -n0 status docker-$service
+done
+
