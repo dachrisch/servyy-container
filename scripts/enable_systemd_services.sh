7cf3a1647786a314febeb6db80b3778b0bb41bc2
dachrisch <christian.daehn@gmail.com>
2022-11-11 13:37:07 +0100

    more fine grained error

--- scripts/enable_systemd_services.sh
+++ scripts/enable_systemd_services.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 system_path=~/.config/systemd/user
-for service in ../systemd/docker-*;do
+for service in ${system_path}/docker-*;do
 	service_name=$( basename $service )
-	service_path=$( realpath $service )
-	
-	ln -s $service_path $system_path/$service_name
+
 	systemctl --user enable $service_name
 done
