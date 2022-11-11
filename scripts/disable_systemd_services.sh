39a2d9a7555b32121fea2ff4afa2883005be4709
dachrisch <christian.daehn@gmail.com>
2022-11-11 13:24:26 +0100

    services are created by ansible now

--- scripts/disable_systemd_services.sh
+++ scripts/disable_systemd_services.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 system_path=~/.config/systemd/user
-for service in ../systemd/docker-*;do
+for service in ${system_path}/docker-*;do
 	service_name=$( basename $service )
-	service_path=$( realpath $service )
-	
+
 	systemctl --user stop $service_name
 	systemctl --user disable $service_name
 done
