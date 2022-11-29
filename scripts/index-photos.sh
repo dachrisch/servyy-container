37f72ddedc1420096e7224474464581654e97fd4
dachrisch <christian.daehn@gmail.com>
2022-11-29 20:59:11 +0100

    no subdir to enable import

--- scripts/index-photos.sh
+++ scripts/index-photos.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 pushd $HOME/servyy-container/photoprism
-current_path=$( date +"cloudy/%Y/%m" )
+current_path=$( date +"%Y/%m" )
 docker-compose exec -T photoprism photoprism index $current_path
