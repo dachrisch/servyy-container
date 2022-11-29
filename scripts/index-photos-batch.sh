63f80d884f308409ce07cf41aa23dc24c079ec5a
dachrisch <christian.daehn@gmail.com>
2022-11-29 21:41:23 +0100

    reindex all files

--- scripts/index-photos-batch.sh
+++ scripts/index-photos-batch.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
+touch $HOME/reindex.log
+truncate -s0 $HOME/reindex.log
 for year in {2004..2022}; do
   for month in {01..12}; do
-    echo "pushd $HOME/servyy-container/photoprism;docker-compose exec -T photoprism photoprism index $year/$month" | batch
+    echo "pushd $HOME/servyy-container/photoprism && docker-compose exec -T photoprism photoprism index $year/$month > $HOME/reindex.log" | batch
   done
 done
