8ef1470e722c10bf2ef02d49c60328c6ee87f68a
dachrisch <christian.daehn@gmail.com>
2023-01-03 14:01:34 +0100

    changed theme

--- social/volumes/pleroma/config.exs
+++ social/volumes/pleroma/config.exs
@@ -1,3 +1,9 @@
 import Config
 
-config :shout enabled: false
+config :shout, enabled: false
+
+config :pleroma, :frontend_configurations,
+  pleroma_fe: %{
+    theme: "pleroma-light",
+    redirectRootNoLogin: "/hello"
+}
