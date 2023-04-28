ca8edcee77411e95772e1d90c02041c2465dc9ce
dachrisch <christian.daehn@gmail.com>
2023-04-28 12:17:25 +0200

    better output

--- scripts/status_systemd_services.sh
+++ scripts/status_systemd_services.sh
@@ -4,5 +4,5 @@ system_path=~/.config/systemd/user
 for service in ${system_path}/docker-*;do
 	service_name=$( basename $service )
 
-	echo "${service_name}...[$(systemctl --user is-active $service_name)]"
+	systemctl --user status --lines=0 --no-pager $service_name
 done
