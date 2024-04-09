c03e729165b7977b169efdeb28cec5d188e0e549
dachrisch <christian.daehn@gmail.com>
2024-04-09 17:03:19 +0200

    cleanup space script

--- scripts/cleanup_space.sh
+++ scripts/cleanup_space.sh
@@ -6,3 +6,6 @@ sudo journalctl --vacuum-size=1G
 echo 'Cleaning up unused docker images...'
 docker image prune -a
 docker system prune -a
+
+echo 'Removing old kernel versions...'
+remove_old_kernels.sh
