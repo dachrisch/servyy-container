391486533058ff6e4d8d8adb50a72fd9ef9c481f
dachrisch <christian.daehn@gmail.com>
2024-04-10 14:22:34 +0200

    create network with correct domain

---
+++ scripts/delete_test_container.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+instance="servyy-test"
+storage_pool=$(yq '.devices.root.pool' $instance.yaml | tr -d '"')
+bridge_network=$(yq '.devices.eth0.parent' $instance.yaml | tr -d '"')
+
+lxc delete --force $instance
+lxc profile delete $instance
+lxc storage delete $storage_pool
+lxc network delete $bridge_network
