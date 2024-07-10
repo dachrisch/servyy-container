6fc7dcb942dcf16308d19fb34cbbd1e8ba013330
dachrisch <christian.daehn@gmail.com>
2024-07-10 15:15:45 +0200

    update firewall

---
+++ hetzner/main.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+
+import json
+import os
+from dataclasses import dataclass
+from typing import Literal, get_args
+
+import fire
+from dataclasses_json import dataclass_json
+from hcloud import Client, APIException
+from hcloud.firewalls import FirewallRule
+
+DIRECTIONS = Literal['in', 'out']
+PROTOCOLS = Literal['udp', 'tcp']
+
+
+@dataclass_json
+@dataclass
+class SerializableRule:
+    direction: str
+    protocol: str
+    source_ips: list[str]
+    port: str | None = None,
+    destination_ips: list[str] | None = None,
+    description: str | None = None,
+
+    @classmethod
+    def from_rule(cls, rule: FirewallRule) -> SerializableRule:
+        return cls(**{slot: getattr(rule, slot) for slot in rule.__slots__})
+
+    def to_rule(self) -> FirewallRule:
+        return FirewallRule(**{slot: getattr(self, slot) for slot in FirewallRule.__slots__})
+
+
+class HetznerFirewall:
+    def __init__(self, client: Client, firewall_name='dns-filtered-fw'):
+        self._firewall = client.firewalls.get_by_name(firewall_name)
+
+    def save(self, dir: str):
+        file_path = f'{os.path.join(dir, self._firewall.name)}_rules.json'
+        sr = list(map(lambda r: SerializableRule.from_rule(r), self._firewall.rules))
+        with open(file_path, 'w') as json_file:
+            json.dump(list(map(lambda x: x.to_dict(), sr)), json_file)
+        print(f'written {len(sr)} rules to {file_path}')
+
+    def merge_update(self, rules_file: str,
+                     description: str,
+                     direction: DIRECTIONS,
+                     protocols: list[PROTOCOLS],
+                     source_ips: list[str],
+                     port: str | None = None,
+                     destination_ips: list[str] = (),
+                     ):
+
+        if direction not in get_args(DIRECTIONS):
+            raise ValueError(f'direction must be one of {get_args(DIRECTIONS)}, was: {direction}')
+        if len(protocols) < 1 or any(p not in get_args(PROTOCOLS) for p in protocols):
+            raise ValueError(f'protocols must be one of {get_args(PROTOCOLS)}, was: {protocols}')
+        if not isinstance(port, str):
+            port = str(port)
+
+        rules = self._load_rules(rules_file)
+
+        print(f'loaded {len(rules)} rules from {rules_file}')
+
+        new_rules = list(filter(lambda r: r.description != description, rules))
+        for protocol in protocols:
+            new_rules.append(
+                SerializableRule(description=description, direction=direction, protocol=protocol, source_ips=source_ips,
+                                 port=port, destination_ips=destination_ips))
+        self._update_rules(new_rules)
+
+    def _update_rules(self, rules):
+        print(f'updating {self._firewall.name} with {len(rules)} rules')
+
+        try:
+            actions = self._firewall.set_rules(list(map(lambda r: r.to_rule(), rules)))
+            for action in actions:
+                print(f'{action.command}: {action.status}')
+        except APIException as e:
+            print(f'ERROR performing firewall update: {e.code}')
+            print(e.message)
+            print(e.details)
+
+    def _load_rules(self, rules_file: str):
+        with open(rules_file, 'r') as json_file:
+            json_list = json.load(json_file)
+            return list(map(lambda x: SerializableRule.from_dict(x), json_list))
+
+    def file_update(self, rules_file: str):
+        pass
+
+
+class HetznerServer:
+    def __init__(self):
+        self._client = Client(token=os.getenv('API_TOKEN'))
+        self.firewall = HetznerFirewall(self._client)
+
+
+if __name__ == '__main__':
+    fire.Fire(HetznerServer)
---
+++ hetzner/update_firewall.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+ipv4=$(curl -s https://ipinfo.io/ip)/32
+ipv6=$(curl -s ifconfig.me/ip | cut -d':' -f -4)::/64
+network=$(iwgetid -r)
+
+if [[ $ipv4 == "/32" ]]; then
+  echo "could not obtain ipv4: $ipv4"
+  exit 1
+fi
+
+if [[ $ipv4 == "::/64" ]]; then
+  echo "could not obtain ipv6: $ipv6"
+  exit 1
+fi
+
+backup_dir=$(mktemp -d)
+python main.py firewall save "$backup_dir"
+echo "updating firewall with ipv4=$ipv4 and ipv6=$ipv6"
+python main.py firewall merge_update "$backup_dir"/dns-filtered-fw_rules.json "Allow DNS from $network" in "('tcp', 'udp')" --port='53' "['$ipv4', '$ipv6']"
+
+echo "checking that DNS is resolving"
+nslookup google.com 88.198.151.84
+nslookup google.com 2a01:4f8:1c1e:d9fb::1
