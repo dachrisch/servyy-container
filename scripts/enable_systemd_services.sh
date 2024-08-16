352c7e3c6da52e5b583fe86d72ff2b1fd0be156d
dachrisch <christian.daehn@gmail.com>
2024-08-16 17:04:31 +0200

    scripts are using docker compose

--- scripts/enable_systemd_services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-system_path=~/.config/systemd/user
-for service in ${system_path}/docker-*;do
-	service_name=$( basename $service )
-
-	systemctl --user enable $service_name
-done
--- scripts/status_systemd_services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-system_path=~/.config/systemd/user
-for service in ${system_path}/docker-*;do
-	service_name=$( basename $service )
-
-	systemctl --user status --lines=0 --no-pager $service_name
-done
